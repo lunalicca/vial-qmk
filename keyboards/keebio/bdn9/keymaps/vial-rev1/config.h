cc6becf1f51df7f8efd153e07cd30d7cd8c85e11
Caroline Tucker <caroline@milkypeach.com>
2021-10-27 13:45:28 -0400

    Added Vial support for Keebio BDN9 Rev. 1

---
+++ keyboards/keebio/bdn9/keymaps/vial-rev1/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xAF, 0xA6, 0xBF, 0xE6, 0xF5, 0x72, 0x0D, 0x38}
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
+
+#define ENCODERS_PAD_A { D0, F6, B6 }
+#define ENCODERS_PAD_B { D1, F5, F7 }
