6bece19b61ba3ddf4615306ee4995b28dda7802f
Caroline Tucker <caroline@milkypeach.com>
2022-05-17 23:14:33 -0400

    Added Vial support for IDOBAO ID75 (/idobo/keymaps/vial)

---
+++ keyboards/idobo/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE0, 0x74, 0x5D, 0xA9, 0xEC, 0x04, 0x8F, 0xA3}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 14 }
